93abdbb153aa5fbf738240c031410c6aea4c0ca0
Denne213 <65536372+Denne213@users.noreply.github.com>
2022-02-22 13:39:01 +0100

    updated point schema

--- src/models/PointSchema.ts
+++ src/models/PointSchema.ts
@@ -3,7 +3,6 @@ import PointInterface from '../interfaces/PointInterface';
 
 export const PointSchema = new Schema<PointInterface>(
     {
-        pointId: { type: Schema.Types.String, required: true, unique: true},
         x: {type: Schema.Types.Number, required: true},
         y: {type: Schema.Types.Number, required: true},
         z: {type: Schema.Types.Number, required: true},

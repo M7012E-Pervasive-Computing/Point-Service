663534df8f92e888ad9df0d9e39f86bd516f4c51
antosalviso <97959752+antosalviso@users.noreply.github.com>
2022-02-22 14:57:40 +0100

    Get Sessions Controller Created

--- src/controllers/SessionController.ts
+++ src/controllers/SessionController.ts
@@ -1,5 +1,9 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import logging from '../config/logging';
 import Controller from './controller';
+import { SessionSchema } from '../models/SessionSchema';
+import ISession from '../interfaces/SessionInterface';
 
 export default class SessionController extends Controller {
   private static instance: SessionController;
@@ -16,7 +20,15 @@ export default class SessionController extends Controller {
   }
 
   public getSessions(req: express.Request, res: express.Response) {
+    logging.info(this.getName(), "Searching for all sessions");
+    let Session = mongoose.model<ISession>('Session', SessionSchema);
+    return Session.find({})
+      .select("name")
+      .then((result) => {
 
-    
+      })
+      .catch((error) => {
+        logging.error(this.getName(), `${error.message}`)
+      })
   }
 }

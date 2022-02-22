19b6b474389004f2e034f518ce4c8238a6920314
erik99s <46327159+erik99s@users.noreply.github.com>
2022-02-22 11:09:25 +0100

    changes in controller for points

--- src/controllers/PointController.ts
+++ src/controllers/PointController.ts
@@ -1,4 +1,6 @@
 import express from 'express';
+import { Session } from 'inspector';
+import logging from '../config/logging';
 import Controller from './controller';
 
 export default class PointController extends Controller {
@@ -7,14 +9,44 @@ export default class PointController extends Controller {
   }
 
   public addPoints(req: express.Request, res: express.Response) {
-    return res.status(200).json({
-      message: 'SUCCESS'
-    });
+    const {session, points} = req.body
+    
+    logging.info(this.getName(), "add points for session");
+    
+    // return Session.findOneAndUpdate(
+    //   {id : id},
+    //   { points },
+    //   { new : true}
+    // )
+    // .exec()
+    // .then((session) => {
+    //   const message
+    // })
+    // return res.status(200).json({
+    //   message: 'SUCCESS'
+    // });
   }
 
   public getSession(req: express.Request, res: express.Response) {
+
+    logging.info(this.getName(), "getting session by Id");
     return res.status(200).json({
       message: 'SUCCESS'
     });
+
+    //find by session id
+
+    // return Session.findById(id)
+    //   .exec()
+    //   .then((session) => {
+    //     return res.status(200).json({
+    //       message: 'SUCCESS'
+    //     });
+    //   })
+    //   .catch((error) => {
+    //     return res.status(500),json({
+
+    //     });
+    //   })
   }
 }

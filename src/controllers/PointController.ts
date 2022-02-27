2d67e54db52e290354e74fc863d595f02a6ed17f
Denne213 <65536372+Denne213@users.noreply.github.com>
2022-02-27 18:27:59 +0100

    updated add point in point controller to use exists

--- src/controllers/PointController.ts
+++ src/controllers/PointController.ts
@@ -19,46 +19,52 @@ export default class PointController extends Controller {
 
   public addPoints(req: express.Request, res: express.Response) {
     const { sessionName, points } = req.body;
-    return Session.findOne({ sessionName: sessionName })
-      .exec()
-      .then((_) => {
-        logging.info(this.getName(), `Attempt to add points for session ${sessionName}`);
-        return Session.updateOne({ sessionName: sessionName }, { $push: { points: { "$each": points} } })
-          .exec()
-          .then((_) => {
-            logging.info(this.getName(), `Successfully added points to session ${sessionName}`);
-            return res.status(200).json({
-              message: `Successfully added points to session ${sessionName}` 
-            });
-          })
-          .catch((error) => {
-            logging.error(this.getName(), `Could not update session ${sessionName} with new points, error: ${error.message}`)
-            return res.status(500).json({
-              message: `Could not add new points to session ${sessionName}, error: ${error.message}`
+    return Session.exists({ sessionName: sessionName })
+      .then((exist) => {
+        if (exist) {
+          logging.info(this.getName(), `Attempt to add points for session ${sessionName}`);
+          return Session.updateOne({ sessionName: sessionName }, { $push: { points: { "$each": points} } })
+            .exec()
+            .then((_) => {
+              logging.info(this.getName(), `Successfully added points to session ${sessionName}`);
+              return res.status(200).json({
+                message: `Successfully added points to session ${sessionName}` 
+              });
             })
-          });
-      })
-      .catch((_) => {
-        logging.info(this.getName(), `Could not find session ${sessionName}, attempt to create a new session`);
-        const session = new Session({
-          sessionName: sessionName,
-          points: points
-        })
+            .catch((error) => {
+              logging.error(this.getName(), `Could not update session ${sessionName} with new points, error: ${error.message}`)
+              return res.status(500).json({
+                message: `Could not add new points to session ${sessionName}, error: ${error.message}`
+              })
+            });
+        } else {
+          logging.info(this.getName(), `Could not find session ${sessionName}, attempt to create a new session`);
+          const session = new Session({
+            sessionName: sessionName,
+            points: points
+          })
 
-        session
-          .save()
-          .then((_) => {
-            logging.info(this.getName(), `Successfully created new session ${sessionName}`);
-            return res.status(201).json({
-              message: `Successfully created a new session ${sessionName}`
-            });
-          })
-          .catch((error) => {
-            logging.error(this.getName(), `Could not create a new session, given error ${error.message}`);
-            return res.status(500).json({
-              message: `Could not add points to existing session or create a new session, error: ${error.message}`
+          session
+            .save()
+            .then((_) => {
+              logging.info(this.getName(), `Successfully created new session ${sessionName}`);
+              return res.status(201).json({
+                message: `Successfully created a new session ${sessionName}`
+              });
             })
-          });
+            .catch((error) => {
+              logging.error(this.getName(), `Could not create a new session, given error ${error.message}`);
+              return res.status(500).json({
+                message: `Could not create a new session, error: ${error.message}`
+              });
+            });
+        }
+      })
+      .catch((error) => {
+        logging.error(this.getName(), `Cannot check if session ${sessionName} exists, error: ${error.message}`);
+        return res.status(500).json({
+          message: `Cannot check if session ${sessionName} exists, error: ${error.message}`
+        });
       });
 
     
